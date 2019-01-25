18515f728b1b98243e5121552b11bd195d3e1ebd
Owen Atkins <owenatkins39@yahoo.com>
2019-01-24 19:52:42 -0500

    Add RotateToAngle
    Add RotateToAngle

---
+++ src/main/java/frc/autonomous/actions/RotateToAngle.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.autonomous.actions;
+
+import frc.robot.subsystems.*;
+import frc.robot.*;
+import frc.autonomous.*;
+import frc.util.*;
+
+/**
+ * Add your docs here.
+ */
+public class RotateToAngle extends Action{
+
+    Bearing b;
+
+	public RotateToAngle(Bearing b, double time) {
+		super("RotateTankAction", time);
+		
+		this.b = b;
+	}
+	
+	@Override
+	public void perform(){
+		
+	}
+}
