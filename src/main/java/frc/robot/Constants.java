bcbb77f1d9eaeaa3de0f035f27b8089de398ccf5
Carl Yoder <carlyodes1@gmail.com>
2019-03-23 12:50:17 -0400

    Recalibrate Front Right Module

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -96,7 +96,7 @@ public class Constants {
 
 	// Practice
 	public static final double kFrontLeftAngleOffset = 144;
-	public static final double kFrontRightAngleOffset = 9;
+	public static final double kFrontRightAngleOffset = 189;
 	public static final double kRearLeftAngleOffset = 244;
 	public static final double kRearRightAngleOffset = 136;
 
